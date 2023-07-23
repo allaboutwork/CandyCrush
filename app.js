a97877afbf4691133decad0f77b55670648021cb
allaboutwork <88450898+allaboutwork@users.noreply.github.com>
2023-07-23 19:42:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const gird = document.querySelector('.grid')
   const width = 8
   const squares = []
+
+  const candyColors = [
+    'red'
+    'yellow'
+    'orange'
+    'purple'
+    'green'
+    'blue'
+  ]
   
   // functions
   // create board
   function createBoard(){
     for(let i = 0; i < width*width; i++){
       const square = document.createElement('div')
+      let randomColor = Math.floor(Math.random() * candyColors.length)
+      square.style.backgroundColor = candyColors[randomColor]   
       grid.appendChild(square)
       squares.push(square)
     }
